0a8bbfd532ff045d03282960ad1ac9cff115dd4c
Melisa Burlando <melisa.burlando@gmail.com>
2023-10-23 15:42:59 -0300

    lista la entity Viaje

--- microservicio-viaje/src/main/java/com/app/viaje/domain/Viaje.java
+++ microservicio-viaje/src/main/java/com/app/viaje/domain/Viaje.java
@@ -1,5 +1,9 @@
 package com.app.viaje.domain;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,16 +26,122 @@ public class Viaje {
 	//cuenta de usuario
 	
 	//fecha de inicio
+	@Column(name="fecha_inicio")
+	private LocalDate fechaInicio;
 	
 	//fecha de fin
+	@Column(name="fecha_fin")
+	private LocalDate fechaFin;
 	
 	//kilometros recorridos
+	@Column(name="kilometros")
+	private Double kilometros;
 	
 	//tarifa basica
+	@Column(name="tarifa_basica")
+	private Double tafifaBasica;
 	
 	//tarifa extra
+	@Column(name="tarifa_extra")
+	private Double tarifaExtra;
 	
 	//boolean enPausa?
 	
 	//tiempo en pausa
+	@Column(name="tiempo_pausa")
+	private LocalTime tiempoPausa; //capaz habría que pasar todos a LocalTime?
+	
+	@Column(name="tiempo_total")
+	private LocalTime tiempoTotal; //resta de fecha fin- inicio
+	
+	//precio total del viaje
+	@Column(name="precio_total")
+	private Double precioTotal; //este es la cuenta de: tarifaBasica*(tiempoTotal-tiempoPausa) + tarifaExtra*tiempoPausa
+
+	public Viaje(Long idViaje, LocalDate fechaInicio, LocalDate fechaFin, Double kilometros, Double tafifaBasica,
+			Double tarifaExtra, LocalTime tiempoPausa, LocalTime tiempoTotal, Double precioTotal) {
+		this.idViaje = idViaje;
+		this.fechaInicio = fechaInicio;
+		this.fechaFin = fechaFin;
+		this.kilometros = kilometros;
+		this.tafifaBasica = tafifaBasica;
+		this.tarifaExtra = tarifaExtra;
+		this.tiempoPausa = tiempoPausa;
+		this.tiempoTotal = tiempoTotal;
+		this.precioTotal = precioTotal;
+	}
+
+	public Long getIdViaje() {
+		return idViaje;
+	}
+
+	public void setIdViaje(Long idViaje) {
+		this.idViaje = idViaje;
+	}
+
+	public LocalDate getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(LocalDate fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public LocalDate getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(LocalDate fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public Double getKilometros() {
+		return kilometros;
+	}
+
+	public void setKilometros(Double kilometros) {
+		this.kilometros = kilometros;
+	}
+
+	public Double getTafifaBasica() {
+		return tafifaBasica;
+	}
+
+	public void setTafifaBasica(Double tafifaBasica) {
+		this.tafifaBasica = tafifaBasica;
+	}
+
+	public Double getTarifaExtra() {
+		return tarifaExtra;
+	}
+
+	public void setTarifaExtra(Double tarifaExtra) {
+		this.tarifaExtra = tarifaExtra;
+	}
+
+	public LocalTime getTiempoPausa() {
+		return tiempoPausa;
+	}
+
+	public void setTiempoPausa(LocalTime tiempoPausa) {
+		this.tiempoPausa = tiempoPausa;
+	}
+
+	public LocalTime getTiempoTotal() {
+		return tiempoTotal;
+	}
+
+	public void setTiempoTotal(LocalTime tiempoTotal) {
+		this.tiempoTotal = tiempoTotal;
+	}
+
+	public Double getPrecioTotal() {
+		return precioTotal;
+	}
+
+	public void setPrecioTotal(Double precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+	
+	
 }
