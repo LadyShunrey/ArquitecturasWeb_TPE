73364426fd7b62b435c24d8812198e328589be01
Melisa Burlando <melisa.burlando@gmail.com>
2023-10-23 03:12:53 -0300

    borro imports sin usar del MonopatinController

--- microservicio-monopatin/src/main/java/com/app/monopatin/controller/MonopatinController.java
+++ microservicio-monopatin/src/main/java/com/app/monopatin/controller/MonopatinController.java
@@ -1,10 +1,9 @@
 package com.app.monopatin.controller;
 
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
